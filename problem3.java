d4c0be1180acbe80b4555dd1d82fa861e0c7c7b8
abhijeet113 <shindeabhijeet113@gmail.com>
2024-01-11 23:51:03 +0530

    problem 3 is started

---
+++ problem3.java
@@ -0,0 +1,19 @@
+/*
+ * Given an integer array nums, find the 
+subarray
+ with the largest sum, and return its sum.
+ */
+public class problem3 {
+    static int[] maxSubArray(int[] nums) {
+        int sum=0;
+        for (int i = 0; i < nums.length; i++) {
+            
+        }
+
+        return nums;
+        
+    }
+    public static void main(String[] args) {
+        
+    }
+}
